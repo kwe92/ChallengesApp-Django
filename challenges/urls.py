8fa91c6f4500cc9ebd4bdbecc57c3c1f8dba1b41
Kwe <Kweayon@gmail.com>
2023-04-06 17:14:46 -0500

    Added Comments

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Dynamic Path Segment:
+    #   - Defined by a capture value, a variable name between angle brackets: <variable_name>
     path('<month>', views.monthlyChallenges)
-    # path('january', views.january),
-    # path('february', views.february),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # URL's Uniform Resource Locator (Routes)
 # - As a developer you have to plan out what URL you want to support
@@ -8,24 +8,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# Views
+# Views (request) => HttpResponse
 #   - a stand alone function that represents the view for that URL
 
-
-def january(request):
-    return HttpResponse('This Works!')
-
-
-def february(request):
-    content = 'Begin, begining is half the work; let half still remain, again begin this and though wilt have finished.'
-    return HttpResponse(content)
-
-# A callback that expects two arguements request && month | where month is the arbitrary name expected in urls.py <month>
+# Dynamic View (request, variable_name) => HttpResponse
+# A callback that expects two arguements request && month | where month is the variable name expected in urls.py <month>
 
 
 def monthlyChallenges(request, month):
     content = _returnVal(month)
-    return HttpResponse(content)
+    if len(content.strip()) > 0:
+        return HttpResponse(content)
+    return HttpResponseNotFound(f'Could not find a page matching {month}')
 
 # A function that acts as a Switch statement
 # Takes in a String month which represents a key of a dictionary and checks the dictionary for said key and if the key exists its value is returned else de
